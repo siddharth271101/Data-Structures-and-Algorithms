6c124ac6ca9a62b22c61809edcc61a7ca28d0919
Siddharth Sudhakar <sidftbll@gmail.com>
2020-08-21 06:39:35 +0530

    simple O(n*n) sol

---
+++ InterviewBit/Arrays/Leaders in an array.cpp
@@ -0,0 +1,18 @@
+https://www.interviewbit.com/problems/leaders-in-an-array/
+
+vector<int> Solution::solve(vector<int> &A) {
+    vector<int> B;
+    for(int i=0;i<A.size();i++){
+        int flag=0;
+        for(int j=i+1;j<A.size();j++){
+            if(A[i]<A[j]){
+            flag=1;
+            break;
+            }
+        }
+        if(flag==0){
+            B.push_back(A[i]);
+        }
+    }
+    return B;
+}
