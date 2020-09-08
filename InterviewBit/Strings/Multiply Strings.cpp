06dd42d447d91d34f4edffbd1bc0f73f6373dd8c
Siddharth Sudhakar <sidftbll@gmail.com>
2020-09-08 22:06:31 +0530

    Very IMP

---
+++ InterviewBit/Strings/Multiply Strings.cpp
@@ -0,0 +1,19 @@
+// https://www.interviewbit.com/problems/multiply-strings/
+
+
+string Solution::multiply(string A, string B) {
+int l1 = A.size();
+        int l2 = B.size();
+        string str( l1 + l2, '0');            
+        for(int i = l1-1; i >= 0; i--){
+            for(int j = l2-1; j >= 0; j--){
+                int p = (B[j]-'0')*(A[i]-'0') + (str[i+j+1]-'0');
+                str[i+j+1] = p%10 + '0';
+                str[i+j] += p/10;
+            }
+        }
+        for(int i = 0; i < l1+l2; i++)
+            if(str[i] != '0')
+                return str.substr(i);
+        return "0";
+        }
