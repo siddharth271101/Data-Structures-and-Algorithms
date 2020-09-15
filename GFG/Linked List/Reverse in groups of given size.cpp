89451c24fb40d6a0eafb7b0f408fd0a00f8be827
Siddharth Sudhakar <sidftbll@gmail.com>
2020-09-15 21:25:14 +0530

    Imp question

---
+++ GFG/Linked List/Reverse in groups of given size.cpp
@@ -0,0 +1,110 @@
+// https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct node *reverse (struct node *head, int k);
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        head = reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+// } Driver Code Ends
+
+
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+struct node *reverse (struct node *head, int k)
+{ 
+    // Complete this method
+    node* curr = head;
+    node* prev = NULL;
+    node* nxt;
+    int count = k;
+    while(count-- && curr!=NULL)
+    {
+        nxt = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    if(head!=NULL)
+        head->next = reverse(nxt,k);
+    return prev;
+    
+}
